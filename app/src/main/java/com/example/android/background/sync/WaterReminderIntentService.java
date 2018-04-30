25dcb8d3d587189baf8523ccc20412bff9ea4c76
Soojeong <soojeongshin2@gmail.com>
2018-04-30 13:29:21 +0900

    T10.01-E-IntentServices

--- app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
+++ app/src/main/java/com/example/android/background/sync/WaterReminderIntentService.java
@@ -1,3 +1,4 @@
+package com.example.android.background.sync;
 /*
  * Copyright (C) 2016 The Android Open Source Project
  *
@@ -14,10 +15,31 @@
  * limitations under the License.
  */
 
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
 // TODO (9) Create WaterReminderIntentService and extend it from IntentService
+public class WaterReminderIntentService extends IntentService {
+
 
 //  TODO (10) Create a default constructor that calls super with the name of this class
+    public WaterReminderIntentService() {
+        super("WaterReminderIntentService");
+    }
 
 //  TODO (11) Override onHandleIntent
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
 //      TODO (12) Get the action from the Intent that started this Service
+        String action = intent.getAction();
+
 //      TODO (13) Call ReminderTasks.executeTaskForTag and pass in the action to be performed
+        if (action != null) {
+            ReminderTasks.executeTask(this, action);
+        }
+
+    }
+}
